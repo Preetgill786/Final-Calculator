4d94324e05ba59f7cb6a3a0f8f3c55e73634e5de
Preetgill786 <57011583+Preetgill786@users.noreply.github.com>
2019-11-06 14:39:49 -0500

    Add files via upload

---
+++ calculator/Round.swift
@@ -0,0 +1,41 @@
+//
+//  Round.swift
+//  calculator
+//
+//  Created by MacStudent on 2019-11-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+import UIKit
+
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
